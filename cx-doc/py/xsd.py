95a30c2b791ae2b66e6b007f382b40814e3dc045
hdknr <hdknr@debsq.hdknr.squ>
2009-12-15 21:36:41 +0900

    XML with Signature node is successfully verified with CX schema

--- cx-doc/py/xsd.py
+++ cx-doc/py/xsd.py
@@ -4,5 +4,5 @@ from lxml import etree
 #
 if __name__ == '__main__':
     (doc,xsd) = [ etree.parse( StringIO( open(f).read())) for f in  sys.argv[1:] ]
-#    print etree.XMLSchema(xsd).validate(doc) 
+    print etree.XMLSchema(xsd).validate(doc) 
     print etree.XMLSchema(xsd).assertValid(doc)
