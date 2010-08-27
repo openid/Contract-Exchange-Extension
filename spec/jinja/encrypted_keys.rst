5358efe1cf7305763e000a548fb554af070c9117
hdknr <hdknr@deblen.hdknr.box>
2010-08-27 12:15:23 +0900

    remove jinja directory

--- spec/jinja/encrypted_keys.rst
+++
@@ -1,4 +0,0 @@
-* type
-
-    * string(URI)
-    * "http://specs.openid.net/cx/1.0/#encrytped_keys"
--- spec/jinja/sample/test.py
+++
@@ -1,5 +0,0 @@
-from cx import *
-from certs import rsa
-
-if __name__ == '__main__':
-    rsa.generate()
