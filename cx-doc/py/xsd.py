6ed129870037067063e0bb535341c6296cdd2841
hdknr <hdknr@deblen.hdknr.box>
2010-11-04 13:35:04 +0900

    restructuring support documents

--- cx-doc/py/xsd.py
+++
@@ -1,8 +0,0 @@
-import sys
-from StringIO import StringIO
-from lxml import etree
-#
-if __name__ == '__main__':
-    (doc,xsd) = [ etree.parse( StringIO( open(f).read())) for f in  sys.argv[1:] ]
-    print etree.XMLSchema(xsd).validate(doc) 
-    print etree.XMLSchema(xsd).assertValid(doc)
--- cx-doc/source/sample/ax/mkinc.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-rm *.inc
-for f in *.xml* ; do
-     echo ".. code-block:: xml" > $f.inc 
-     echo "" >> $f.inc
-     cat $f  | while read line  ; do echo "    $line" >> $f.inc ; done
-done
